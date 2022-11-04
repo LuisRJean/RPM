7f2212487a9d9732783946a3f302468a33a96068
Luis Jean <luisjeanknowsbest@gmail.com>
2022-11-04 00:53:43 -0400

    updated main\

--- Rust_Project/src/main.rs
+++ Rust_Project/src/main.rs
@@ -4,24 +4,13 @@ use std::io;
 use std::io::{Write, BufReader, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
-fn print_str(x: String){
-    println!("A string: {}", x);
-}
-
-
-fn print_return_str(x: String) -> String{
-    println!("A string {}", x);
-    x
-}
-
-fn change_string(name: & mut String){
-    name.push_str("is happy");
-    println!("Message: {}", name);
-}
 
 fn main() {
-    let mut str1 = String::from("Luis");
-    change_string(&mut str1);
+    let mut heroes = HashMap::new();
+    heroes.insert("SUperman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    println!("{:?}", heroes);
 }
  
